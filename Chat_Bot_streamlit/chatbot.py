c13618c140a1c443615096847479fcda7bb8a9ea
mandartule <mandartule5@gmail.com>
2026-08-18 00:01:55 +0530

    multi modal added

---
+++ Chat_Bot_streamlit/chatbot.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv 
+import streamlit as st
+from langchain_openai import ChatOpenAI
+
+#load env variables
+load_dotenv()
+
+# streamlit page setup
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🦁",
+    layout="centered",
+)
+st.title("💬 Generative AI Chatbot")
+
+# initiate chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# llm initiate
+llm = ChatOpenAI(
+    base_url="https://openai.generative.engine.capgemini.com/v1",
+    model="openai.gpt-4o",  # or "anthropic.claude-haiku-4-5-20251001-v1:0"
+    temperature=1
+)
+
+# input box
+user_prompt = st.chat_input("Ask Chatbot...")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    ## this system prompt is not shown to the user its only added to the list so that the model can understand the context
+    ## done by unpacking the history and creating a new list by adding the first message
+    response = llm.invoke(
+        input = [{"role": "system", "content": "You are a helpful assistant"}, *st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
